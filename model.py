9f8fc1b054ef3a0abfe8aefe84dd9e9f7043ada2
Bernardo <btbennemann@gmail.com>
2025-11-30 20:49:28 -0300

    upgrading model

--- model.py
+++ model.py
@@ -5,9 +5,25 @@ class Model(nn.Module):
     def __init__(self, num_features_entrada):
         super(Model, self).__init__()
 
-        self.linear = nn.Linear(num_features_entrada, 1)
+        self.layer1 = nn.Linear(num_features_entrada, 64)
+        
+        self.layer2 = nn.Linear(64, 64)
+
+        self.output = nn.Linear(64, 1)
+        
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.2)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        linear_output = self.linear(x)
-        probability = torch.sigmoid(linear_output)
-        return probability
+        x = self.layer1(x)
+        x = self.relu(x) 
+        x = self.dropout(x)
+
+        x = self.layer2(x)
+        x = self.relu(x)
+
+        x = self.output(x)
+        x = self.sigmoid(x)
+
+        return x
